cda5d9163d9490915131e45089514cf1bd704aef
Louie Middle <louie.middle@hawkeyeinnovations.com>
2021-11-04 12:38:07 +0000

    FDP-130: aws-s3-zipper output to s3

--- aws-s3-zipper/README.md
+++ aws-s3-zipper/README.md
@@ -1,9 +1,12 @@
 # Using aws-s3-zipper
 
--   Modified package to use profile credentials instead of hardcoding them
--   Much faster than the multi-part upload I was using (TODO figure out why)
+-   Defaults to being told access keys
+-   Similar speed to the java application I was wrote
 -   Makes seperate zips however for each part (I'm still using a 1000 files)
--   Also loses file structure, so all files just get dumped in the zip
--   Means there's some logic there. Also probably want to fork and modify the aws-s3-zipper as required (like I did for the credentials thing)
--   This is to add things like specifying the output bucket etc.
--   This little tool seems good, only think is it's written in JavaScript, but it shouldn't be so complicated that not everyone can work on it
+-   Also loses file structure in the zip, so all files just get dumped in the zip
+-   Can't specify output bucket or path.
+
+-   Because of these limitations I forked the repo, and made it use my okta profile credentials.
+-   Also made it able to specify output bucket.
+
+This package seems good, only think is it's written in JavaScript, but it shouldn't be so complicated that not everyone can work on it. Ultimately we would still have to write an application and run it somewhere to do the zipping, so it might be easier to write a Java command line app so that it's something everyone is more familiar with?
--- aws-s3-zipper/test.js
+++ aws-s3-zipper/test.js
@@ -25,47 +25,44 @@ var zipper = new S3Zipper(config);
 //     }
 // );
 
-zipper.zipToFileFragments(
-    // Max file size is that of the ZIP not the files going in the zip
-    // I couldn't  seem to get max file count to work, not sure why.
-    {
-        s3FolderName:
-            "messages/2022/1_UEFA Champions League/53106_Old Trafford/2032650_Man. United_Atalanta/2021-10-20/1800/delayed.samples.people.joints",
-        startKey: null,
-        zipFileName: "./myLocalFile.zip",
-        maxFileSize: 1024 * 1024 * 50,
-    },
-    function (err, results) {
-        if (err) console.error(err);
-        else {
-            if (results.length > 0) {
-                var result = results[results.length - 1];
-                var lastFile =
-                    result.zippedFiles[result.zippedFiles.length - 1];
-                if (lastFile) console.log("last key ", lastFile.Key); // next time start from here
-            }
-        }
-    }
-);
-
-// TODO need to figure out how to specify output bucket
-
-// zipper.zipToS3FileFragments(
+// zipper.zipToFileFragments(
+//     // Max file size is that of the ZIP not the files going in the zip
+//     // I couldn't  seem to get max file count to work, not sure why.
 //     {
 //         s3FolderName:
-//             "fdp-feed-data-stage/messages/2022/1_UEFA Champions League/53106_Old Trafford/2032650_Man. United_Atalanta/2021-10-20/1800/delayed.samples.people.joints/",
-//         startKey: null, // optional
-//         s3ZipFileName: "myS3File.zip",
-//         maxFileCount: 1000,
-//         maxFileSize: 1024 * 1024,
-//         tmpDir: "/tmp", // optional, defaults to node_modules/aws-s3-zipper
+//             "messages/2022/1_UEFA Champions League/53106_Old Trafford/2032650_Man. United_Atalanta/2021-10-20/1800/delayed.samples.people.joints",
+//         startKey: null,
+//         zipFileName: "./myLocalFile.zip",
+//         maxFileSize: 1024 * 1024 * 50,
 //     },
 //     function (err, results) {
 //         if (err) console.error(err);
-//         else if (results.length > 0) {
-//             var result = results[results.length - 1];
-//             var lastFile = result.zippedFiles[result.zippedFiles.length - 1];
-//             if (lastFile) console.log("last key ", lastFile.Key); // next time start from here
+//         else {
+//             if (results.length > 0) {
+//                 var result = results[results.length - 1];
+//                 var lastFile =
+//                     result.zippedFiles[result.zippedFiles.length - 1];
+//                 if (lastFile) console.log("last key ", lastFile.Key); // next time start from here
+//             }
 //         }
 //     }
 // );
+
+zipper.zipToS3FileFragments(
+    {
+        s3FolderName:
+            "messages/2022/1_UEFA Champions League/53106_Old Trafford/2032650_Man. United_Atalanta/2021-10-20/1800/delayed.samples.people.joints",
+        startKey: null, // optional
+        s3ZipFileName: "myS3File.zip",
+        maxFileSize: 1024 * 1024,
+        bucket: "louiem-test",
+    },
+    function (err, results) {
+        if (err) console.error(err);
+        else if (results.length > 0) {
+            var result = results[results.length - 1];
+            var lastFile = result.zippedFiles[result.zippedFiles.length - 1];
+            if (lastFile) console.log("last key ", lastFile.Key); // next time start from here
+        }
+    }
+);
